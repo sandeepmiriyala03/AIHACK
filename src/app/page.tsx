c83163b3e2ecec5385890747fe42594ae56b3487
Sandeep Miriyala <3719057+sandeepmiriyala03@users.noreply.github.com>
2025-08-22 22:36:39 +0530

    text changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function About() {
         <h1 className="title" style={{ marginBottom: "1rem" }}>Welcome to MultiDecode!</h1>
         <div style={{ fontSize: "1.1rem", color: "#333" }}>
           <div>• Easily upload images and documents for text extraction.</div>
-          <div>• Support for over 38 languages with powerful OCR technology.</div>
+          <div>• Support for over 34 languages with powerful OCR technology.</div>
           <div>• Quickly analyze and process your documents with a user-friendly interface.</div>
            <div>• Install the app for a smoother and enhanced experience.</div>
         </div>
