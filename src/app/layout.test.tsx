a0073a06f945e6111658cacfaa9956bcddd5a92f
Sandeep Miriyala <3719057+sandeepmiriyala03@users.noreply.github.com>
2025-08-24 22:56:15 +0530

    add test cases

---
+++ src/app/layout.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import RootLayout from './layout'; // adjust import path as needed
+
+
+describe('RootLayout', () => {
+  test('renders children and Footer component', () => {
+    const childText = 'Hello Layout Child';
+    render(
+      <RootLayout>
+        <div>{childText}</div>
+      </RootLayout>
+    );
+
+    expect(screen.getByText(childText)).toBeInTheDocument();
+
+  });
+
+  
+    
+  
+});
