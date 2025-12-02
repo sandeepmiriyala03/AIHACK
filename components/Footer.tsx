3594031888894040fc18078ff7ab7b18b98a7249
Sandeep Miriyala <3719057+sandeepmiriyala03@users.noreply.github.com>
2025-12-02 23:47:36 +0530

    footer imporved

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,49 +1,50 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-
 import styles from "../Styles/Footer.module.css";
 
 export default function Footer() {
-  const currentYear = new Date().getFullYear();
-  const [currentTime, setCurrentTime] = useState("");
+  const year = new Date().getFullYear();
+  const [timeIST, setTimeIST] = useState("");
 
   useEffect(() => {
-    const updateTime = () => {
-      const options: Intl.DateTimeFormatOptions = {
+    const updateIST = () => {
+      const formatter = new Intl.DateTimeFormat("en-IN", {
         hour: "2-digit",
         minute: "2-digit",
+        second: "2-digit",
         hour12: false,
         timeZone: "Asia/Kolkata",
-      };
-      const time = new Intl.DateTimeFormat("en-IN", options).format(new Date());
-      setCurrentTime(time);
+      });
+      setTimeIST(formatter.format(new Date()));
     };
 
-    updateTime();
-    const interval = setInterval(updateTime, 1000);
-    return () => clearInterval(interval);
+    updateIST();
+    const timer = setInterval(updateIST, 1000);
+    return () => clearInterval(timer);
   }, []);
 
-  const lastDeployed = process.env.NEXT_PUBLIC_LAST_DEPLOYED || "N/A";
+  const deployedDate = process.env.NEXT_PUBLIC_LAST_DEPLOYED || "N/A";
 
   return (
     <footer className={styles.footer}>
-      <p className={styles.attribution}>
-    
-        <span className={styles.symbol}>🤖</span>
-        Yukthisala AI Lab &nbsp;&nbsp;
-        <span className={styles.symbol}>👨‍💻</span>
-        Sandeep Miriyala&nbsp;&nbsp;
-     ⚛️&nbsp;&nbsp; 📱 &nbsp;&nbsp;
-        &nbsp; © {currentYear} &nbsp;&nbsp; IST: {currentTime} &nbsp;
-        <span className={styles.flagWrapper}>
-        
-        </span>
-      </p>
-     <p className={styles.deployment}>
-  <span className={styles.deploymentSymbol}>📅 </span> Deployed on: <span className={styles.deploymentDate}>{lastDeployed}</span>
-</p>
+      {/* Branding Row */}
+      <div className={styles.row}>
+        <span className={styles.icon}>🤖</span> Yukthisala AI Lab &nbsp; | &nbsp;
+        <span className={styles.icon}>👨‍💻</span> Sandeep Miriyala &nbsp; | &nbsp;
+        ⚛️ Mobile + AI Engineering
+      </div>
+
+      {/* Time + Year */}
+      <div className={styles.row}>
+        © {year} • IST Time: <strong>{timeIST}</strong>
+      </div>
+
+      {/* Deployment Info */}
+      <div className={styles.deployRow}>
+        <span className={styles.deployIcon}>📅</span>
+        Last Deployed: <span className={styles.deployDate}>{deployedDate}</span>
+      </div>
     </footer>
   );
 }
