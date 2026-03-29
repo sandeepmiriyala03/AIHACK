3f701f07adab6e49e2782961f7bc92b48c9ad353
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-03-29 13:45:18 +0530

    ok code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -80,8 +80,7 @@ const navGroups = [
   {
     group: "Voice AI", 
     items: [
-      { href: "/voice", label: "Talking AI", icon: MicRoundedIcon },
-      { href: "/ebook", label: "Pdf to Voice", icon: MicRoundedIcon }
+      { href: "/voice", label: "Talking AI", icon: MicRoundedIcon }
     ],
   },
 ];
